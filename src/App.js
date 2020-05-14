025f532876137646c24e53f0552137c46f9958ce
Parija Malgaonkar <parijamalgaonkar@gmail.com>
2020-05-15 03:14:47 +0530

    changes made

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
-import React from 'react';
+import React, {Component} from 'react';
 import CardList from './CardList.js';
 import Searchbox from './Searchbox'
 import {identityy} from './Identities.js';
 
-const App = () => {
-	return (
-		<div className = 'tc'>
-			<h1> Autobots </h1>
-			<Searchbox />
-			<CardList identityy={identityy}/>
-		</div>
-	);
+const state = {
+
+}  
+
+
+
+class App extends Component  {
+	constructor() {
+		super()
+		this.state = {
+			identityy : identityy,
+			searchfield : ''
+		}
+	}
+	onSearchChange = (event) => {
+		this.setState({searchfield: event.target.value});
+	}
+	render() {
+			const filtered = this.state.identityy.filter(identityy => {
+			return identityy.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+		})
+		return (
+			<div className = 'tc'>
+				<h1> Autobots </h1>
+				<Searchbox searchChange = {this.onSearchChange}/>
+				<CardList identityy={filtered}/>
+			</div>
+		);
+	}
 }
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App.js'
 import * as serviceWorker from './serviceWorker'; 
 import 'tachyons';
-import {identityy} from './Identities.js';
+
 
 ReactDOM.render(
   <React.StrictMode>
